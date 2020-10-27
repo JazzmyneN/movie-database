7dad1e8fced32390e58a57e8601f1a5bd177072b
Ryan Kinney <rkinney220@west-mec.org>
2020-10-27 14:00:56 -0700

    attempted the API & created JS files

---
+++ movie.js
@@ -0,0 +1,6 @@
+let result = new XMLHttpRequest();
+
+result.open("GET", "https://api.themoviedb.org/3/genre/movie/list?api_key=669f2553f52c85815c5e7c10b3197af7&language=en-US");
+result.send();
+
+
