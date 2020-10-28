2f918096fc6f865859c7cdcf0903b9ad24579148
Ryan Kinney <rkinney220@west-mec.org>
2020-10-28 13:56:05 -0700

    attempting to fix API

--- TV.js
+++ TV.js
@@ -1,4 +1,7 @@
-let result = new XMLHttpRequest();
+// 669f2553f52c85815c5e7c10b3197af7
+
+
+let xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = () => {
     const data = JSON.parse(xhr.response);
@@ -16,5 +19,5 @@ xhr.onreadystatechange = () => {
     document.querySelector('.tv_slide_three').innerHTML = data.results[2].overview;
     document.querySelector('.thirdm').src = "https://image.tmdb.org/t/p/original/" + data.results[2].poster_path;
 }
-result.open("GET", "https://api.themoviedb.org/3/genre/movie/list?api_key=669f2553f52c85815c5e7c10b3197af7&language=en-US");
-result.send();
+xhr.open("GET", "https://api.themoviedb.org/3/genre/movie/list?api_key=669f2553f52c85815c5e7c10b3197af7&language=en-US");
+xhr.send();
